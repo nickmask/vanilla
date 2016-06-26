626622f9374ecd16678d7433c214096e541274e4
Nick Maskell <nrmaskell@gmail.com>
2016-06-26 17:11:18 +1200

    add listening and mouseover event

--- utils/create-table.js
+++ utils/create-table.js
@@ -1,15 +1,22 @@
 export default function (size) {
   const tbl = document.createElement('table')
   const tblBody = document.createElement('tbody')
-
   for (let i = 0; i < size; i++) {
     const row = document.createElement('tr')
     for (let j = 0; j < size; j++) {
       const cell = document.createElement('td')
+      cell.addEventListener('mouseover', handleEvent)
+      cell.setAttribute('id', `x${i}y${j}`)
       row.appendChild(cell)
     }
     tblBody.appendChild(row)
   }
   tbl.appendChild(tblBody)
   document.body.appendChild(tbl)
+}
+
+
+function handleEvent (e) {
+  const cell = e.target.id
+  document.getElementById(cell).className = 'active'
 }
